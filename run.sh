95acfeb789dd701049210fda6d7703bdc1725968
ycw517 <ycw517@gmail.com>
2020-06-08 00:34:03 +0800

    fux run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 JOB_DIR=/home/pi/Project/C001_PttCrawler/
+LOG_DIR=/home/pi/Project/C001_PttCrawler/
+if [ -z "$1" ]
+then
+    LOG_DIR=/home/pi/Project/C001_PttCrawler/
+else
+    LOG_DIR=$1
+fi
 #python3  ${JOB_DIR}MainJob.py 1 ${JOB_DIR}config.ini > /media/nfs/log/cronlog$(date +"%Y%m%d%H%M%S") 2>&1
-python3  ${JOB_DIR}MainJob.py 1 ${JOB_DIR}config.ini > /media/nfs/log/cronlog 2>&1
+python3  ${JOB_DIR}MainJob.py 1 ${JOB_DIR}config.ini > ${LOG_DIR}cronlogpj 2>&1
